da81c5c5591b44c01cd8800c03cac26d9ada5bd9
courtney-oconnell <63523222+courtney-oconnell@users.noreply.github.com>
2020-04-11 21:04:10 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,8 +12,6 @@ function setup() {
     clouds[i] = new cloud(); 
   }
 
-  bee = createSprite(windowWidth/2, windowHeight/2);
-  bee.scale(.5);
   bee = createSprite(windowWidth/2, windowHeight/2, .5, .5);
   bee.addImage(beeImg);
 }
@@ -27,8 +25,6 @@ function setup() {
       clouds[i].display();
     }
 
-    //bee.position.x = mouseX;
-    //bee.position.y = mouseY;
     drawSprites();
 }
 
