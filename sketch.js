f619bd71780b08fd2890c35255894c624d1c0441
courtney-oconnell <63523222+courtney-oconnell@users.noreply.github.com>
2020-04-16 14:21:04 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup() {
   }
 
   for (var i = 0; i < 3; i++) {
-      bee = createSprite(random(10, (windowWidth-50)), random(10, (windowHeight-100)));
+      bee = createSprite(random(10, (windowWidth-25)), random(10, (windowHeight-25)));
       bee.addAnimation("fly", beeAnim);
       beeAnim.play();
     }
