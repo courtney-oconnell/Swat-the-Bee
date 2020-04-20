d595e34ce413691ce70ce07bb2d386ce1cc61eab
courtney-oconnell <63523222+courtney-oconnell@users.noreply.github.com>
2020-04-20 17:37:07 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -14,71 +14,58 @@ let isVideo = false;
 let model = null;
 
 const modelParams = {
-  flipHorizontal: true, // flip e.g for video  
-  maxNumBoxes: 2, // maximum number of boxes to detect
-  iouThreshold: 0.5, // ioU threshold for non-max suppression
-  scoreThreshold: 0.6, // confidence threshold for predictions.
+    flipHorizontal: true,   // flip e.g for video  
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.6,    // confidence threshold for predictions.
 }
 
 function startVideo() {
-  handTrack.startVideo(video).then(function(status) {
-    console.log("video started", status);
-    if (status) {
-      updateNote.innerText = "Video started. Now tracking"
-      isVideo = true
-      runDetection()
-    } else {
-      updateNote.innerText = "Please enable video"
-    }
-  });
+    handTrack.startVideo(video).then(function (status) {
+        console.log("video started", status);
+        if (status) {
+            updateNote.innerText = "Video started. Now tracking"
+            isVideo = true
+            runDetection()
+        } else {
+            updateNote.innerText = "Please enable video"
+        }
+    });
 }
 
 function toggleVideo() {
-  if (!isVideo) {
-    updateNote.innerText = "Starting video"
-    startVideo();
-  } else {
-    updateNote.innerText = "Stopping video"
-    handTrack.stopVideo(video)
-    isVideo = false;
-    updateNote.innerText = "Video stopped"
-  }
+    if (!isVideo) {
+        updateNote.innerText = "Starting video"
+        startVideo();
+    } else {
+        updateNote.innerText = "Stopping video"
+        handTrack.stopVideo(video)
+        isVideo = false;
+        updateNote.innerText = "Video stopped"
+    }
 }
 
-let h1x, h1y, h2x, h2y;
+
 
 function runDetection() {
-  model.detect(video).then(predictions => {
-
-    if (predictions[0] != null) {
-      // for (var a in predictions[0]) {
-      //   console.log(predictions[0][a]);
-      // }
-
-      h1x = predictions[0].bbox[0];
-      h1y = predictions[0].bbox[1];
-
-      if (predictions[1] != null) {
-        h2x = predictions[1].bbox[0];
-        h2y = predictions[1].bbox[1];
-      }
-
-    }
-
-    model.renderPredictions(predictions, canvas, context, video);
-    if (isVideo) {
-      requestAnimationFrame(runDetection);
-    }
-  });
+    model.detect(video).then(predictions => {
+        console.log("Predictions: ", predictions);
+        model.renderPredictions(predictions, canvas, context, video);
+        if (isVideo) {
+            requestAnimationFrame(runDetection);
+        }
+    });
 }
 
 // Load the model.
 handTrack.load(modelParams).then(lmodel => {
-  // detect objects in the image.
-  model = lmodel
-  updateNote.innerText = "Loaded Model!"
-  trackButton.disabled = false
+    // detect objects in the image.
+    model = lmodel
+    updateNote.innerText = "Loaded Model!"
+    trackButton.disabled = false
 });
+
+
 
 
 function preload() {
