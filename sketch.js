e7273c084680b2b7a213ab5694451bbd33a47e23
courtney-oconnell <63523222+courtney-oconnell@users.noreply.github.com>
2020-04-21 11:19:55 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -85,7 +85,7 @@ handTrack.load(modelParams).then(lmodel => {
 
 function preload() {
   beeAnim = loadAnimation("bees/Bee_1b.png", "bees/Bee_2b.png", "bees/Bee_3b.png", "bees/Bee_4b.png", "bees/Bee_5b.png");
-  handAnim = loadAnimation("hand.png");
+  handAnim = loadAnimation("hand2.png");
 }
 
 function setup() {
