1a05caee4378d08a784e305f63e4d0a8644be197
courtney-oconnell <63523222+courtney-oconnell@users.noreply.github.com>
2020-04-27 16:12:16 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -89,7 +89,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   //map(windowWidth, 0, 640, 0, windowWidth);
   //map(windowHeight, 0, 480, 0, windowHeight);
-  map(h1x, 0, 640, 0, windowWidth);
+  map(model, 0, 640, 0, windowWidth);
 
 
   for (var i = 0; i < 20; i++) { 
