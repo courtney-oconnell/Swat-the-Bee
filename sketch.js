4eec578f8f5299ea0d975b8f06eb998cc0cd1866
courtney-oconnell <63523222+courtney-oconnell@users.noreply.github.com>
2020-04-28 19:41:05 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -95,27 +95,23 @@ function setup() {
     clouds[i] = new cloud(); 
   }
 
-  for(var i = 0; i < 3; i++){
-    bugs[i] = new bees;
-  }
+  var x1 = random(30, 610);
+  var y1 = random(30, 450);
+  bee = createSprite(x1, y1);
+  bee.addAnimation("fly", beeAnim);
+  beeAnim.play();
 
-  //var x1 = random(30, 610);
-  //var y1 = random(30, 450);
-  //bee = createSprite(x1, y1);
-  //bee.addAnimation("fly", beeAnim);
-  //beeAnim.play();
+  var x2 = random(30, 610);
+  var y2 = random(30, 450);
+  bee1 = createSprite(x2, y2);
+  bee1.addAnimation("fly", beeAnim);
+  beeAnim.play();
 
-  //var x2 = random(30, 610);
-  //var y2 = random(30, 450);
-  //bee1 = createSprite(x2, y2);
-  //bee1.addAnimation("fly", beeAnim);
-  //beeAnim.play();
-
-  //var x3 = random(30, 610);
-  //var y3 = random(30, 450);
-  //bee2 = createSprite(x3, y3);
-  //bee2.addAnimation("fly", beeAnim);
-  //beeAnim.play();
+  var x3 = random(30, 610);
+  var y3 = random(30, 450);
+  bee2 = createSprite(x3, y3);
+  bee2.addAnimation("fly", beeAnim);
+  beeAnim.play();
 }
 
  function draw() {
@@ -128,10 +124,6 @@ function setup() {
       clouds[i].display();
     }
 
-  for (var i = 0; i < bees.length; i++) {
-      bees[i].move();
-      bees[i].display();
-    }
     drawSprites();
 
     fill("red");
@@ -160,29 +152,6 @@ function backs() {
     fill('#2FC969')
     //grassHeight =  480 - (.5 * 480);
     rect(0, 0, 640, -220); 
-}
-
-function bees(){
-  var bee;
-
-  this.x = random(30, 610);
-  this.y = random(30, 450);
-
-  this.display = function() {
-    bee = createSprite(x1, y1);
-    bee.addAnimation("fly", beeAnim);
-    beeAnim.play();
-    //drawSprites();
-  }
-
-    this.move = function() {
-    this.x = this.x += 1 ;
-    this.y = this.y + random(-1, 1);
-    
-    if(this.x >= windowWidth){
-      this.x = 0;
-    }
-  }
 }
 
 function cloud(){
